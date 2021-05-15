184c106fc91c122713dc9e1632f21a4b9095f482
Jose Luco <j_luco@hotmail.com>
2021-05-15 19:32:50 -0300

    cambios varios

---
+++ js/mail.js
@@ -0,0 +1,12 @@
+let datos = [
+    {titulo: "Carne", cantidad: 12, precio: 3000},
+    {titulo: "Jabon", cantidad: 15, precio: 2500},
+    {titulo: "Shampoo", cantidad: 9, precio: 2000},
+    {titulo: "Coca", cantidad: 5, precio: 1000}
+
+
+]
+
+let vista = []
+let datosPrevios = []
+
